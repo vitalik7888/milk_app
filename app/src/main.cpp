96a8c7a234839db717a23393566615e1282a8aa5
Vitaliy <myprotonant@protonmail.com>
2017-12-15 10:58:15 +0300

    Logging to a file

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -7,8 +7,44 @@
 #include <QDebug>
 
 
+QTextStream logStream;
+
+void milkMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtInfoMsg:
+        logStream << "INFO: " << localMsg.constData();
+        fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtWarningMsg:
+        logStream << "WARNING: " << localMsg.constData();
+        fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtCriticalMsg:
+        logStream << "CRITICAL: " << localMsg.constData();
+        fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtFatalMsg:
+        logStream << "FATAL: " << localMsg.constData();
+        fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        abort();
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    QFile data("milk.log");
+    if (data.open(QFile::WriteOnly | QFile::Truncate)) {
+        logStream.setDevice(&data);
+        qInstallMessageHandler(milkMessageOutput);
+    } else {
+        qWarning() << "Cannot open file for logging";
+    }
+
     QApplication app(argc, argv);
     QCoreApplication::setApplicationName(Constants::appName());
     QCoreApplication::setOrganizationName(Constants::organization());
